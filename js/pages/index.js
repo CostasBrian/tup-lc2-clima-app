6ea411eaee213dc0fcadc189f4690fafcc63299b
202120212020 <brian.costas.2014@gmail.com>
2022-06-15 09:26:55 -0300

    llamada Api funcionando

--- js/pages/index.js
+++ js/pages/index.js
@@ -20,11 +20,12 @@ const seleccionCiudad = document.getElementById("seleccionable")
 
 
 async function ConsultarDatos() {
-    const key = 'cf3c32eca5c309cab34ab9b732fd66a3'
-    const respApi = await fetch('http://api.openweathermap.org/data/2.5/weather?q={seleccionCiudad.value}&appid={key}&units=metric&lang=es', {
-        method: "get"
+    console.log(seleccionCiudad.value)
+    const respApi = await fetch('https://api.openweathermap.org/data/2.5/weather?q=rosario&appid=cf3c32eca5c309cab34ab9b732fd66a3&units=metric&lang=es', {
+        method: "get",
     })
     return respApi
+    console.log(respApi.json())
 }
 
 botonConsultar.addEventListener("click", async() => {

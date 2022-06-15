f1f3b87fc917883c1697ea1353da180c2c0f768f
CostasBrian <brian.costas.2014@gmail.com>
2022-06-14 22:26:46 -0300

    renegando con timeout

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,22 +1,76 @@
 const botonAgregar = document.getElementById("agregar") /*recibo valor de boton*/
 const ciudad = document.getElementById("ciudad_a_agregar") /*recibo valor de input de ciudad*/
-
+    //----------------------------------------------------------------------------------------------------------------------------
+    //funcion para comprobar el estado del storage
 function leerLocal() {
-    let lista = localStorage.getItem("Lista_Ciudades")
+    debugger
+    let lista = localStorage.getItem("Lista_Ciudades") /*leo el contenido de sessionstorage y lo ingreso en una lista*/
+    lista = JSON.parse(lista) /*convierte a array*/
+    let band = true
     if (lista) {
-        lista = JSON.parse(lista)
+        for (let i = 0; i < lista.length; i++) {
+            if (lista[i] == ciudad.value) {
+                band = false
+                break;
+            }
+        }
+        if (band == false) {
+            return found_mesage()
+        }
     } else {
-        lista = []
+        lista = [] /*sino se crea la lista*/
     }
-    return lista
+    return lista //devuelve la lista convertida para en arreglo para agregarle la ciudad
+}
+//---------------------------------------------------------------------------------------------------------------------------
+//fubcion para cargar las ciudades al storage 
+function cargarLocal() {
+    let ciudades = leerLocal() /*leo la lista generada en otra funcion */
+    ciudades.push(ciudad.value) /*agrego la ciudad a la lista*/
+    localStorage.setItem("Lista_Ciudades", JSON.stringify(ciudades)); /*fijo los elementos al session storage */
+    exito_mesage()
+    time()
+    limpiarInput()
 }
 
-function cargarLocal() { /*guardo en local storage*/
-    let ciudades = leerLocal()
-    ciudades.push(ciudad.value)
-    localStorage.setItem("Lista_Ciudades", JSON.stringify(ciudades));
+//----------------------------------------------------------------------------------------------------------------------------
+//funcion para limpiar el input
+function limpiarInput() {
+    ciudad.value = ""
+}
+//----------------------------------------------------------------------------------------------------------------------------
+function error_mesage() {
+    document.getElementById("error").style.display = "block"
 }
 
+function hide_error_mesage() {
+    document.getElementById("error").style.display = "none"
+}
+//----------------------------------------------------------------------------------------------------------------------------
+function exito_mesage() {
+    document.getElementById("succes").style.display = "block"
+}
 
+function hide_exito_mesage() {
+    document.getElementById("succes").style.display = "none"
+}
+//----------------------------------------------------------------------------------------------------------------------------
+function found_mesage() {
+    document.getElementById("found").style.display = "block"
 
+}
+
+function hide_found_mesage() {
+    document.getElementById("found").style.display = "block"
+
+}
+//----------------------------------------------------------------------------------------------------------------------------
+
+function time() {
+    setTimeout(exito_mesage(), {.hide(6000);
+        },
+    }
+    2000)
+}
+//----------------------------------------------------------------------------------------------------------------------------
 botonAgregar.addEventListener("click", cargarLocal) /*escucho el click de agregar*/
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,10 +1,11 @@
-ciudad_get = localStorage.getItem("Lista_Ciudades") /*leo localstorgae*/
+ciudad_get = localStorage.getItem("Lista_Ciudades") /*leo sessionstorge*/
 lista = JSON.parse(ciudad_get)
 console.log(lista)
 
+/*bucle para crear las opciones a medida q se ingresa una ciudad*/
 for (i = 0; i <= lista.length; i++) {
     const select = document.querySelector('select') /*leo elemento html*/
-    const opcion = document.createElement("option") /*crea elemento opcion */
+    const opcion = document.createElement("option") /*creo elemento opcion */
     opcion.value = lista[i]
     opcion.textContent = lista[i] /*asigno valor a elemento opcion*/
     select.appendChild(opcion) /*coloco el opcion como child del select*/
